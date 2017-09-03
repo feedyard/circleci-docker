7a5965975995192464ae6c5d8148f45bdea9e5d1
Nic Cheneweth <nchenewe@thoughtworks.com>
2017-09-03 17:14:46 -0500

    updateing configuration tests

--- profiles/circleci-docker/controls/circleci-docker.rb
+++ profiles/circleci-docker/controls/circleci-docker.rb
@@ -3,19 +3,38 @@ control 'packages' do
   title 'confirm package installation'
   desc 'confirm all desired packages are installed'
   describe command('apk info') do
-    its('stdout') { should include ('ca-certificates') }
     its('stdout') { should include ('git') }
     its('stdout') { should include ('openssh') }
+    its('stdout') { should include ('tar') }
+    its('stdout') { should include ('gzip') }
+    its('stdout') { should include ('ca-certificates') }
+    its('stdout') { should include ('bash') }
+    its('stdout') { should include ('bash-doc') }
+    its('stdout') { should include ('bash-completion') }
     its('stdout') { should include ('curl') }
     its('stdout') { should include ('wget') }
-    its('stdout') { should include ('bash') }
-    its('stdout') { should include ('openrc') }
     its('stdout') { should include ('openssl') }
+    its('stdout') { should include ('openrc') }
     its('stdout') { should include ('python3') }
-    its('stdout') { should include ('docker') }
     its('stdout') { should include ('ruby') }
+    its('stdout') { should include ('ruby-bundler') }
+    its('stdout') { should include ('ruby-dev') }
     its('stdout') { should include ('g++') }
+    its('stdout') { should include ('libffi-dev') }
+    its('stdout') { should include ('musl-dev') }
     its('stdout') { should include ('make') }
+    its('stdout') { should include ('docker') }
+  end
+end
+
+control 'python packages' do
+  impact 1.0
+  title 'confirm python package installation'
+  desc 'confirm all desired python packages are installed'
+  describe command('pip list') do
+    its('stdout') { should include ('invoke') }
+    its('stdout') { should include ('boto3') }
+    its('stdout') { should include ('awscli') }
   end
 end
 
@@ -25,6 +44,42 @@ control 'git version' do
   desc 'confirm version reported by git matches the desired version'
   describe command('git version') do
     its('stdout') { should include ('2.13') }
+  end
+end
+
+control 'openssh version' do
+  impact 1.0
+  title 'confirm openssh version installed'
+  desc 'confirm version reported by openssh matches the desired version'
+  describe command('ssh -V') do
+    its('stderr') { should include ('7.5p1') }
+  end
+end
+
+control 'tar version' do
+  impact 1.0
+  title 'confirm tar version installed'
+  desc 'confirm version reported by tar matches the desired version'
+  describe command('tar --version') do
+    its('stdout') { should include ('1.29') }
+  end
+end
+
+control 'gzip version' do
+  impact 1.0
+  title 'confirm gzip version installed'
+  desc 'confirm version reported by gzip matches the desired version'
+  describe command('gzip --version') do
+    its('stdout') { should include ('1.8') }
+  end
+end
+
+control 'bash version' do
+  impact 1.0
+  title 'confirm bash version installed'
+  desc 'confirm version reported by bash matches the desired version'
+  describe command('bash --version') do
+    its('stdout') { should include ('4.3.48') }
   end
 end
 
@@ -46,21 +101,21 @@ control 'wget version' do
   end
 end
 
-control 'bash version' do
-  impact 1.0
-  title 'confirm bash version installed'
-  desc 'confirm version reported by bash matches the desired version'
-  describe command('bash --version') do
-    its('stdout') { should include ('4.3.48') }
-  end
-end
-
 control 'openssl version' do
   impact 1.0
   title 'confirm openssl version installed'
   desc 'confirm version reported by openssl matches the desired version'
   describe command('openssl version') do
     its('stdout') { should include ('1.0.2') }
+  end
+end
+
+control 'openrc version' do
+  impact 1.0
+  title 'confirm openrc version installed'
+  desc 'confirm version reported by openrc matches the desired version'
+  describe command('openrc -V') do
+    its('stdout') { should include ('0.24.1') }
   end
 end
 
@@ -73,29 +128,20 @@ control 'python3 version' do
   end
 end
 
-control 'pip version' do
+control 'ruby version' do
   impact 1.0
-  title 'confirm pip version installed'
-  desc 'confirm version reported by pip3 matches the desired version'
-  describe command('pip -V') do
-    its('stdout') { should include ('9.0.1') }
+  title 'confirm ruby version installed'
+  desc 'confirm version reported by ruby matches the desired version'
+  describe command('ruby -v') do
+    its('stdout') { should include ('2.4.1') }
   end
 end
 
-control 'docker version' do
+control 'bundler version' do
   impact 1.0
-  title 'confirm docker version installed'
-  desc 'confirm version reported by docker matches the desired version'
-  describe command('docker -v') do
-    its('stdout') { should include ('17.05.0-ce') }
-  end
-end
-
-control 'docker-compose version' do
-  impact 1.0
-  title 'confirm docker-compose version installed'
-  desc 'confirm version reported by docker-compose matches the desired version'
-  describe command('docker-compose -v') do
+  title 'confirm bundler version installed'
+  desc 'confirm version reported by bundler matches the desired version'
+  describe command('bundler -v') do
     its('stdout') { should include ('1.15') }
   end
 end
@@ -118,21 +164,39 @@ control 'make version' do
   end
 end
 
-control 'ruby version' do
+control 'docker version' do
   impact 1.0
-  title 'confirm ruby version installed'
-  desc 'confirm version reported by ruby matches the desired version'
-  describe command('ruby -v') do
-    its('stdout') { should include ('2.4.1') }
+  title 'confirm docker version installed'
+  desc 'confirm version reported by docker matches the desired version'
+  describe command('docker -v') do
+    its('stdout') { should include ('17.05.0-ce') }
   end
 end
 
-control 'bundler version' do
+control 'pip version' do
   impact 1.0
-  title 'confirm bundler version installed'
-  desc 'confirm version reported by bundler matches the desired version'
-  describe command('bundler -v') do
-    its('stdout') { should include ('1.15') }
+  title 'confirm pip version installed'
+  desc 'confirm version reported by pip3 matches the desired version'
+  describe command('pip -V') do
+    its('stdout') { should include ('9.0.1') }
+  end
+end
+
+control 'docker-compose version' do
+  impact 1.0
+  title 'confirm docker-compose version installed'
+  desc 'confirm version reported by docker-compose matches the desired version'
+  describe command('docker-compose -v') do
+    its('stdout') { should include ('1.16') }
+  end
+end
+
+control 'invoke version' do
+  impact 1.0
+  title 'confirm invoke version installed'
+  desc 'confirm version reported by invoke matches the desired version'
+  describe command('invoke -V') do
+    its('stdout') { should include ('0.20') }
   end
 end
 
@@ -141,15 +205,6 @@ control 'inspec version' do
   title 'confirm inspec version installed'
   desc 'confirm version reported by inspec matches the desired version'
   describe command('inspec version') do
-    its('stdout') { should include ('1.33.1') }
-  end
-end
-
-control 'invoke version' do
-  impact 1.0
-  title 'confirm invoke version installed'
-  desc 'confirm version reported by invoke matches the desired version'
-  describe command('invoke --version') do
-    its('stdout') { should include ('0.20.3') }
+    its('stdout') { should include ('1.35') }
   end
 end
