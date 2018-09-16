00b5ee73154028fffdcb6117222aa0db798268a2
ncheneweth <nchenewe@thoughtworks.com>
2018-09-15 21:13:01 -0500

    testing

--- testlocal.sh
+++ testlocal.sh
@@ -2,5 +2,6 @@
 set -euo pipefail
 
 CID="$(docker run -it -d --entrypoint ash local/circleci-remote-docker:latest)"
+inspec exec --no-distinct-exit profiles/cis-docker
 inspec exec profiles/circleci-remote-docker/ -t docker://$CID
 docker rm -f $CID
